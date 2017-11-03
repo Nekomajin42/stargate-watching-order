06d0f248595e4b02083d75860b79eccca16a7566
Nekomajin42 <nekomajin42@gmail.com>
2017-11-03 15:45:23 +0100

    v0.4

--- script.js
+++ script.js
@@ -3,21 +3,32 @@
 window.addEventListener("DOMContentLoaded", function()
 {
 	const marks = document.querySelectorAll("sup a");
-	const popup = document.querySelector("#popup");
 	for (let i=0; i<marks.length; i++)
 	{
-		marks[i].addEventListener("mouseover", function(e)
+		marks[i].addEventListener("mouseenter", function(e)
 		{
 			let id = e.target.id.substring(e.target.id.indexOf("-") + 1);
+			let popup = document.createElement("div");
+			popup.id = "popup";
 			popup.innerHTML = document.querySelector("#note-" + id).innerHTML;
-			popup.style.left = (window.scrollX + window.innerWidth - e.pageX < 430) ? e.pageX - 410 + "px" : e.pageX + 10 + "px";
-			popup.style.top = (window.scrollY + window.innerHeight - e.pageY < 130) ? e.pageY - 110 + "px" : e.pageY + 10 + "px";
+			document.body.appendChild(popup);	
+			popup.style.left = (window.scrollX + window.innerWidth - e.pageX < popup.clientWidth) ? 
+				e.pageX - popup.clientWidth - 10 + "px" : 
+				e.pageX + 10 + "px";
+			popup.style.top = (window.scrollY + window.innerHeight - e.pageY < popup.clientHeight) ? 
+				e.pageY - popup.clientHeight - 10 + "px" : 
+				e.pageY + 10 + "px";
 			popup.classList.add("visible");
 		});
 		
 		marks[i].addEventListener("mouseleave", function()
 		{
+			let popup = document.querySelector("#popup");
 			popup.classList.remove("visible");
+			window.setTimeout(function()
+			{
+				document.body.removeChild(popup);
+			}, 300);
 		});
 		
 		marks[i].addEventListener("click", function(e)
